7a3bddff75fa69bb877ea62bf995b5b6f58884b8
Nicholas Seay <nicholasseay@DN0a2267be.SUNet>
2019-05-12 16:35:36 -0700

    cropped image now white on black instead of the opposite

--- image_cropper.py
+++ image_cropper.py
@@ -77,29 +77,8 @@ def main():
 
     # Read back in as a numpy array
     img = imageio.imread(to_filename)
-
-    # Check if we need conversion to greyscale
-    # if len(img.shape) > 2:
-
-    #     img = img[:, :, :3] # pull rgb channels
-
-    #     if np.max(img, 2).any() > 1: # we're in the 0-255 scale
-
-    #         img = img / 255
-
-    #     c_linear_r = 0.2126 * img[:,:,0]
-    #     c_linear_g = 0.7152 * img[:,:,1]
-    #     c_linear_b = 0.0722 * img[:,:,2]
-
-    #     c_linear = c_linear_r + c_linear_g + c_linear_b
-
-    #     print(c_linear.shape)
-
-    #     img = 12.92 * c_linear * np.where(c_linear <= 0.0031308, 1, 0) \
-    #           + (1.055 * (c_linear ** (1/2.4)) - 0.055) * np.where(c_linear > 0.0031308, 1, 0)
-
-
-    #     imageio.imsave(to_filename, img)
+    img = 255 - img
+    imageio.imsave(to_filename, img)
 
     test(filename, to_filename)
 
